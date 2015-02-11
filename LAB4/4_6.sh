a465fd26ecd231c655c4ef6c933fa3f5643b8201
varshateckchandani <varsha.teckchandani@iet.ahduni.edu.in>
2015-02-11 20:45:04 +0530

    Update 4_6.sh

--- LAB4/4_6.sh
+++ LAB4/4_6.sh
@@ -2,10 +2,10 @@ hour=$(date +"%H")
 minute=$(date +"%M")
 second=$(date +"%S")
 
-if [ $hour -lt 12 ]
+if [ $hour -lt 12 -a $hour -ge 8 ]
 then 
 	echo "Good morning !!!"
-elif [ $hour -lt 16 ]
+elif [ $hour -lt 16 -a $hour -ge 12 ]
 then
 	echo "Good afternoon!!!"
 else 
